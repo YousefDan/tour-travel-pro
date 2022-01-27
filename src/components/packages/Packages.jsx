314ad96115ba17e2a81d8d160a4110d8f11e891f
YousefDan <yousifpapa@gmail.com>
2022-01-27 22:28:30 +0330

    Packages component adde4d

--- src/components/packages/Packages.jsx
+++ src/components/packages/Packages.jsx
@@ -1,9 +1,37 @@
+import { packagesData } from "../../data";
 import Heading from "../heading/Heading";
+import "./packages.scss"
 
 const Packages = () => {
-    return ( <section className="packages">
-        <Heading title="Our Packages" />
-    </section> );
-}
- 
+  return (
+    <section className="packages">
+      <Heading title="Our Packages" />
+      <div className="packages-container">
+        {packagesData.map((item) => (
+          <div key={item.id} className="package-item">
+            <div className="img-wrapper">
+              <img src={item.img} alt="" />
+              <div className="destination">
+                <i className="fas fa-map-marker-alt"></i>
+                {item.destination}
+              </div>
+            </div>
+            <div className="info-wrapper">
+              <div className="price">
+                {item.priceWithDiscount}
+                <span>{item.price}</span>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad,
+                nam!
+              </p>
+              <div className="btn">Book Now</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
 export default Packages;
---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const packagesData = [
+    {
+        id: 1,
+        img:"/images/p-1.jpg",
+        destination: "Mumbai",
+        price: "350.99",
+        priceWithDiscount: "290.99",
+    },
+    {
+        id: 2,
+        img:"/images/p-2.jpg",
+        destination: "Hawai",
+        price: "250.99",
+        priceWithDiscount: "200.99",
+    },
+    {
+        id: 3,
+        img:"/images/p-3.jpg",
+        destination: "Sydney",
+        price: "290",
+        priceWithDiscount: "240",
+    },
+    {
+        id: 4,
+        img:"/images/p-4.jpg",
+        destination: "Paris",
+        price: "250",
+        priceWithDiscount: "240.99",
+    },
+    {
+        id: 5,
+        img:"/images/p-5.jpg",
+        destination: "Tokyo",
+        price: "310",
+        priceWithDiscount: "280.99",
+    },
+    {
+        id: 6,
+        img:"/images/p-6.jpg",
+        destination: "Egypt",
+        price: "310",
+        priceWithDiscount: "280.99",
+    },
+]
